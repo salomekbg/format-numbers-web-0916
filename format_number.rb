b7fc33b9f62a12c859c23ccda117647eb2b7a394
SluvsP <salomekbg@gmail.com>
2017-01-17 17:26:17 -0500

    Done.

--- format_number.rb
+++ format_number.rb
@@ -1,3 +1,8 @@
 def separate_with_comma(num)
-  # code goes here
+  new_num = num.to_s
+  num_length = new_num.length
+  while num_length > 3
+    new_num.insert(num_length -= 3, ",")
+  end
+  new_num
 end
